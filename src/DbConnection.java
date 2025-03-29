02ab9a01cc4b1dfed2b745713b671a8123e9f1b0
LAKSHMAN-ECE <161059568+LAKSHMAN-ECE@users.noreply.github.com>
2025-03-29 16:49:29 +0530

    Update DbConnection.java

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class DbConnection {
     private static final  String url="jdbc:mysql://localhost:3306/busresv";
     private static final    String username="root";
-    private static final  String password="Tlpbu29@";
+    private static final  String password="abcd";
 
     public static Connection getConnection() throws Exception{
         return DriverManager.getConnection(url,username,password);
